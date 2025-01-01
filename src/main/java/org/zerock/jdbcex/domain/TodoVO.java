960901bb35e879b7b04f4ebb0fdbe1275bb0833b
user <thdjun92@naver.com>
2025-01-01 17:53:30 +0900

    tododao test 반영

---
+++ src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -0,0 +1,21 @@
+package org.zerock.jdbcex.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+public class TodoVO {
+
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+}
