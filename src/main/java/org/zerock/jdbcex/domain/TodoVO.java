5ae3837709d4508d9fcfc56d5fd9ec848b512214
user <thdjun92@naver.com>
2025-05-06 15:43:20 +0900

    TodoService 실습

--- src/main/java/org/zerock/jdbcex/domain/TodoVO.java
+++ src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -1,14 +1,14 @@
 package org.zerock.jdbcex.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter //읽기전용이니 getter 만 추가
 @Builder // 생성시 패턴
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class TodoVO {
 
     private Long tno;
---
+++ src/test/java/org/zerock/jdbcex/service/TodoServiceTest.java
@@ -0,0 +1,30 @@
+package org.zerock.jdbcex.service;
+
+import jdk.jpackage.internal.Log;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dto.TodoDTO;
+
+import java.time.LocalDate;
+
+@Log4j2
+class TodoServiceTest {
+
+    private TodoService todoService;
+
+    @BeforeEach
+    public void ready() {
+        todoService = TodoService.INSTANCE;
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("JDBC Test Title")
+                .dueDate(LocalDate.now())
+                .build();
+
+        todoService.register(todoDTO);
+    }
+}
