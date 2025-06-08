25f9e63b111cbb3eda0e73dfa054ecfc1a427751
omea92 <thdjun92@naver.com>
2025-06-08 13:22:56 +0900

    jpa crud repository junit example

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTest.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTest.java
@@ -6,6 +6,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
 @Log4j2
@@ -58,5 +62,12 @@ class BoardRepositoryTest {
         Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending()); //0~10 bno 기준 내림차순
+
+        Page<Board> result = boardRepository.findAll(pageable);
     }
 }
