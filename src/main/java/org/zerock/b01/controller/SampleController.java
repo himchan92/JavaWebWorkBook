24cb31b18b322bf630efb5a8fa7fcba66ee7a671
omea92 <thdjun92@naver.com>
2025-06-08 09:19:05 +0900

    chapter 05 spring boot sample example

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,5 +16,12 @@ public class SampleController {
         log.info("hello.....................");
 
         model.addAttribute("msg", "HELLO WORLD");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+
+        model.addAttribute("list", list);
     }
 }
