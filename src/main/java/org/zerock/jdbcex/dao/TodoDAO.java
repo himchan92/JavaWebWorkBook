b427ab0976c6de6af6c38ed83597b21f82ad04d9
omea92 <thdjun92@naver.com>
2025-10-08 21:38:43 +0900

    jdbc dao

--- src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
+++ src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -3,6 +3,7 @@ package org.zerock.jdbcex.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import lombok.Cleanup;
 
 public class TodoDAO {
 
@@ -19,6 +20,19 @@ public class TodoDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return now;
+    }
+
+    public String getTime2() throws Exception {
+        // Cleanup : try-catch문 대신 close까지 보장되는 롬복지원
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
 
         return now;
     }
